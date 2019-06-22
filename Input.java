efcadd53b8fd0d90f609c60a235732e730337743
PHILLY D <51379518+JamesAndAdrien@users.noreply.github.com>
2019-06-22 16:09:41 +0100

    Update Input.java

--- Input.java
+++ Input.java
@@ -1,45 +1,77 @@
 package gameLoop;
 
 import java.awt.event.KeyEvent;
-
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
-import javax.swing.JFrame;
-
-public class Input extends JFrame {
-	
-	public static final int NUM_KEYCODES = 256;
-	
+public class Input implements KeyListener {
+		
 	private static ArrayList<Integer> currentKeys = new ArrayList<Integer>();
 	
+	private static ArrayList<Integer> justPressedKeys = new ArrayList<Integer>();
+	
+	private static ArrayList<Integer> justReleasedKeys = new ArrayList<Integer>();
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+		
+		
+	}
+
+	@Override
 	public void keyPressed(KeyEvent e) {
 		
+		if(!justPressedKeys.contains(e.getKeyCode()) && !currentKeys.contains(e.getKeyCode())) {
+			
+			justPressedKeys.add(new Integer(e.getKeyCode()));
+			
+		}
 		
-		
+		if (!currentKeys.contains(e.getKeyCode())) {
+			
+			currentKeys.add(new Integer (e.getKeyCode()));
+			
+		}
+					
 	}
-	
+
+	@Override
 	public void keyReleased(KeyEvent e) {
 		
-		
-		
-	}
-	
-	public static void update() {
-		
-		
+		currentKeys.remove(new Integer (e.getKeyCode()));
 		
 	}
 	
-	public static boolean getKey(int keyCode) {
+	public static boolean getKeyDown(int keyCode) {
 		
-		return false;
+		if (justPressedKeys.contains(keyCode)) {
+			
+			return true;
+			
+		}
+		
+		else {
+			
+			return false;
+			
+		}
 		
 	}
 	
-	public static boolean getKeyPressed(int keyCode) {
+	public static boolean getKeyUp(int keyCode) {
 		
+		if (!currentKeys.contains(keyCode)) {
+			
+			return true;
+			
+		}
 		
-		return false;
-	}
-	
+		else {
+			
+			return false;
+			
+		}
+		
+	}	
 }
